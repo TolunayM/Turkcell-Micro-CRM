a49aebcba62ff08387eb56a9ffa2e41915bee2d2
Tolunay <tlnymtlu@gmail.com>
2024-05-13 03:35:33 +0300

    new dto

--- CustomerService/src/main/java/com/TurkcellSRS/CustomerService/DTO/Requests/CustomerRequests/AddCustomerRequest.java
+++ CustomerService/src/main/java/com/TurkcellSRS/CustomerService/DTO/Requests/CustomerRequests/AddCustomerRequest.java
@@ -1,18 +1,36 @@
 package com.TurkcellSRS.CustomerService.DTO.Requests.CustomerRequests;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class AddCustomerRequest {
-    private Long NationalityId;
+
+    @Digits(integer = 11, fraction = 0) @Positive
+    @NotNull(message = "This field is required")
+    private Long nationalityId;
+
+    @NotBlank(message = "This field is required") @Size(max=50)
     private String firstName;
+
+    @Size(max = 50) @Pattern(regexp = "^[a-zA-Z0-9_]*[a-zA-Z_]*[a-zA-Z0-9_]*$")
     private String middleName;
+    @NotBlank(message = "This field is required") @Size(max = 50)  @Pattern(regexp = "^[a-zA-Z0-9_]*[a-zA-Z_]*[a-zA-Z0-9_]*$")
     private String lastName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    @NotNull(message = "This field is required")
     private Date birthDate;
+    @NotBlank(message = "This field is required")
     private String gender;
-    private String motherName;
+    @Size(max = 50) @Pattern(regexp = "^[a-zA-Z0-9_]*[a-zA-Z_]*[a-zA-Z0-9_]*$")
     private String fatherName;
+
+    @Size(max = 50) @Pattern(regexp = "^[a-zA-Z0-9_]*[a-zA-Z_]*[a-zA-Z0-9_]*$")
+    private String motherName;
 }
